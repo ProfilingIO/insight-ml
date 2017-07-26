280b44e6ba838c8a5affcc48b0c2cc3cf2cbb958
Stefan Henß <stefan.henss@gmail.com>
2017-07-26 05:36:56 +0200

    Simplified SplitGain class

--- insight-ml-core/src/main/java/com/insightml/data/features/stats/SplitGain.java
+++ insight-ml-core/src/main/java/com/insightml/data/features/stats/SplitGain.java
@@ -15,22 +15,62 @@
  */
 package com.insightml.data.features.stats;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.insightml.data.samples.ISamples;
 import com.insightml.models.trees.ISplit;
 import com.insightml.models.trees.MseSplitCriterion;
 import com.insightml.models.trees.SplitFinderContext;
 import com.insightml.models.trees.ThresholdSplitFinder;
+import com.insightml.utils.Arrays;
+import com.insightml.utils.Collections;
+import com.insightml.utils.Collections.SortOrder;
+import com.insightml.utils.jobs.ParallelFor;
+import com.insightml.utils.ui.UiUtils;
+import com.insightml.utils.ui.reports.IUiProvider;
 
-public final class SplitGain extends AbstractIndependentFeatureStatistic {
+public final class SplitGain implements IFeatureStatistic, IUiProvider<ISamples<?, Double>> {
+	private static final Logger LOG = LoggerFactory.getLogger(SplitGain.class);
 
 	@Override
-	protected double compute(final FeatureStatistics stats, final int feature, final String featureName) {
+	public final Map<String, Double> run(final FeatureStatistics stats) {
+		final long start = System.currentTimeMillis();
+		final ThresholdSplitFinder thresholdSplitFinder = createSplitFinder(stats);
+
+		final String[] feats = stats.getInstances().featureNames();
+		final Double[] result = Arrays.of(ParallelFor.run(i -> {
+			return Double.valueOf(compute(i, thresholdSplitFinder));
+		}, 0, feats.length, 1));
+		final Map<String, Double> map = new HashMap<>(feats.length);
+		for (int i = 0; i < feats.length; ++i) {
+			map.put(feats[i], result[i]);
+		}
+
+		LOG.info("Computed statistics in {} ms", Long.valueOf(System.currentTimeMillis() - start));
+		return map;
+	}
+
+	private static ThresholdSplitFinder createSplitFinder(final FeatureStatistics stats) {
 		final SplitFinderContext context = new SplitFinderContext(stats.getInstances(), 10, 10, stats.getLabelIndex());
 		final boolean[] subset = new boolean[stats.getInstances().size()];
 		for (int i = 0; i < subset.length; ++i) {
 			subset[i] = true;
 		}
-		final ISplit split = ThresholdSplitFinder.createThresholdSplitFinder(context, subset, MseSplitCriterion::create)
-				.apply(feature);
+		return ThresholdSplitFinder.createThresholdSplitFinder(context, subset, MseSplitCriterion::create);
+	}
+
+	private static double compute(final int feature, final ThresholdSplitFinder thresholdSplitFinder) {
+		final ISplit split = thresholdSplitFinder.apply(feature);
 		return split == null ? 0 : split.getImprovement() / split.getWeightSum();
+	}
+
+	@Override
+	public String getText(final ISamples<?, Double> instances, final int labelIndex) {
+		final FeatureStatistics stats = new FeatureStatistics(instances, labelIndex);
+		return UiUtils.toString(Collections.sort(run(stats), SortOrder.DESCENDING), true, false);
 	}
 }
