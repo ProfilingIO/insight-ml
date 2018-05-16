e3648b9dacd3e620bbeeab223ac4389498f1f260
Stefan Henß <stefan.henss@gmail.com>
2018-05-16 06:53:52 +0200

    Bagging: fix logger name

--- src/main/java/com/insightml/models/meta/Bagging.java
+++ src/main/java/com/insightml/models/meta/Bagging.java
@@ -38,7 +38,7 @@ import com.insightml.utils.Arguments;
 import com.insightml.utils.IArguments;
 
 public class Bagging<I extends Sample> extends AbstractEnsembleLearner<I, Double, Double> {
-	private transient Logger LOG = LoggerFactory.getLogger(Bagging.class);
+	private transient Logger LOG = LoggerFactory.getLogger(getClass());
 
 	private final VoteStrategy strategy;
 
