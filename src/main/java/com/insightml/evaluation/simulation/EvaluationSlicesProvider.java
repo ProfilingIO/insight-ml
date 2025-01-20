a51f594e4b5504f97fae1afdb185af3dad9bad03
Stefan Henß <stefan@bd4travel.com>
2025-01-20 10:36:30 +0100

    Fix license header

--- src/main/java/com/insightml/evaluation/simulation/EvaluationSlicesProvider.java
+++ src/main/java/com/insightml/evaluation/simulation/EvaluationSlicesProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Stefan Henß
+ * Copyright (C) 2016 Stefan Henß
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
